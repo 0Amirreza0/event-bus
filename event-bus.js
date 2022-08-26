466e7cd5b831cc5bccd3eb86c1589f584eafb7fd
0Amirreza0 <amirreza.saadati.c@gmail.com>
2022-08-26 22:41:39 +0430

    feat: add publish and unsubscribe methods

--- event-bus.js
+++ event-bus.js
@@ -1,55 +1,141 @@
+/**
+ * @callback SubscriptionCallback
+ * @param {...*} args
+ */
+
+/**
+ * @typedef {Object} Subscriber
+ * @property {string} id
+ * @property {?SubscriptionCallback} callback
+ * @property {?Function} unsubscribe
+ */
+
+/**
+ * @typedef {Object} GetSubscriberMapOptions
+ * @param {boolean} [createIfAbsent=false]
+ */
+
 import deepClone from './utilities/deep-clone.js';
 
 class EventBus {
-  #eventsSubscriptionMap;
+  #eventsSubscriberMap;
 
+  /**
+   * Create a new EventBus
+   */
   constructor() {
-    this.#eventsSubscriptionMap = {};
+    this.#eventsSubscriberMap = {};
   }
 
-  _getEvent(eventName, { createIfAbsent = false } = {}) {
+  /**
+   * @param {string} eventName
+   * @param { GetSubscriberMapOptions } options
+   * @returns {?Object.<Symbol, Subscriber>} Subscriber map for the event
+   */
+  _getEventSubscriberMap(eventName, { createIfAbsent = false } = {}) {
     if (!eventName) throw new Error('eventName is required');
 
-    let subscriptionMap = this.#eventsSubscriptionMap[eventName];
+    const subscriptionMap = this.#eventsSubscriberMap[eventName];
 
     if (!subscriptionMap && createIfAbsent)
-      this.#eventsSubscriptionMap[eventName] = {};
+      this.#eventsSubscriberMap[eventName] = {};
 
-    return this.#eventsSubscriptionMap[eventName];
+    return this.#eventsSubscriberMap[eventName];
   }
 
+  /**
+   *
+   * @param {string} eventName
+   * @returns {?Array.<Subscriber>} List of subscribers
+   */
+  _getEventSubscribersAsList = (eventName) => {
+    const subscriberMap = this._getEventSubscriberMap(eventName);
+    if (!subscriberMap) return;
+
+    const subscriberIdList = Object.getOwnPropertySymbols(subscriberMap);
+
+    return subscriberIdList.map((subscriberId) => ({
+      callback: subscriberMap[subscriberId],
+      id: subscriberId,
+    }));
+  };
+
+  /**
+   *
+   * @param {string} eventName
+   * @returns {?Object.<Symbol, Subscriber>} Subscriber map for the event
+   */
+  _garbageCollector(eventName) {
+    const subscriberList = this._getEventSubscribersAsList(eventName);
+    if (!subscriberList) return;
+
+    const subscriberMap = {};
+    let isGarbageEvent = true;
+
+    subscriberList.forEach((subscriber) => {
+      const { id, callback } = subscriber;
+      const hasCallback = !!callback;
+
+      if (!hasCallback) return;
+
+      subscriberMap[id] = callback;
+      isGarbageEvent = false;
+    });
+
+    if (!isGarbageEvent)
+      return (this.#eventsSubscriberMap[eventName] = subscriberMap);
+
+    const { [eventName]: garbageEvent, ...otherEvents } =
+      this.#eventsSubscriberMap;
+
+    this.#eventsSubscriberMap = otherEvents;
+  }
+
+  /**
+   *
+   * @param {string} eventName
+   * @param {Symbol} subscriptionId
+   */
   _unsubscribe(eventName, subscriptionId) {
-    console.log('unsubscribing ', subscriptionId);
-    //TODO: Implement unsubscribe
+    const subscriberMap = this._getEventSubscriberMap(eventName);
+
+    subscriberMap[subscriptionId] = null;
+    this._garbageCollector(eventName);
   }
 
+  /**
+   *
+   * @param {string} eventName
+   * @param {Function} callback
+   * @returns {Subscriber} A new subscriber
+   */
   subscribeOn(eventName, callback) {
-    const event = this._getEvent(eventName, { createIfAbsent: true });
+    const subscriberMap = this._getEventSubscriberMap(eventName, {
+      createIfAbsent: true,
+    });
     const subscriptionId = Symbol(eventName);
 
-    event[subscriptionId] = callback;
+    subscriberMap[subscriptionId] = callback;
 
     return {
       id: subscriptionId,
-      eventName,
       unsubscribe: () => this._unsubscribe(eventName, subscriptionId),
     };
   }
 
   // TODO: subscribeOnceOn
 
+  /**
+   *
+   * @param {string} eventName
+   * @param  {...any} args
+   */
   publish(eventName, ...args) {
-    const event = this._getEvent(eventName);
+    const subscriberList = this._getEventSubscribersAsList(eventName) ?? [];
 
-    if (!event) throw new Error(`Found no event named: ${eventName}`);
-
-    const subscriberIdList = Object.getOwnPropertySymbols(event);
-
-    subscriberIdList.forEach((subscriberId) => {
-      const subscription = event[subscriberId];
-
+    subscriberList.forEach((subscriber) => {
       const subscriptionArguments = deepClone(args);
-      subscription(...subscriptionArguments);
+      subscriber.callback(...subscriptionArguments);
     });
   }
 }
