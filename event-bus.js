3da6055fda6836bf401d1ade20768b9bb18d3f23
0Amirreza0 <amirreza.saadati.c@gmail.com>
2022-08-26 15:57:50 +0430

    feat: implement publish method

--- event-bus.js
+++ event-bus.js
@@ -1,3 +1,5 @@
+import deepClone from './utilities/deep-clone.js';
+
 class EventBus {
   #eventsSubscriptionMap;
 
@@ -6,6 +8,8 @@ class EventBus {
   }
 
   _getEvent(eventName, { createIfAbsent = false } = {}) {
+    if (!eventName) throw new Error('eventName is required');
+
     let subscriptionMap = this.#eventsSubscriptionMap[eventName];
 
     if (!subscriptionMap && createIfAbsent)
@@ -14,7 +18,7 @@ class EventBus {
     return this.#eventsSubscriptionMap[eventName];
   }
 
-  _unsubscribe(subscriptionId) {
+  _unsubscribe(eventName, subscriptionId) {
     console.log('unsubscribing ', subscriptionId);
     //TODO: Implement unsubscribe
   }
@@ -26,11 +30,28 @@ class EventBus {
     event[subscriptionId] = callback;
 
     return {
-      unsubscribe: () => this._unsubscribe(subscriptionId),
+      id: subscriptionId,
+      eventName,
+      unsubscribe: () => this._unsubscribe(eventName, subscriptionId),
     };
   }
 
   // TODO: subscribeOnceOn
+
+  publish(eventName, ...args) {
+    const event = this._getEvent(eventName);
+
+    if (!event) throw new Error(`Found no event named: ${eventName}`);
+
+    const subscriberIdList = Object.getOwnPropertySymbols(event);
+
+    subscriberIdList.forEach((subscriberId) => {
+      const subscription = event[subscriberId];
+
+      const subscriptionArguments = deepClone(args);
+      subscription(...subscriptionArguments);
+    });
+  }
 }
 
-module.exports = EventBus;
+export default EventBus;
